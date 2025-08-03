20271a314f585c25a641f5d80b4b2b6d5515c8a6
dc10r <cg.david.0406@gmail.com>
2025-08-02 19:13:26 -0600

    'Version1.3.2'

---
+++ src/app/pages/interrumpcion-eventos/interrumpcion-eventos.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal, Input } from '@angular/core';
+import {PuntosComponent} from '../puntos/puntos.component';
+
+@Component({
+  selector: 'app-interrumpcion-eventos',
+  imports: [PuntosComponent],
+  templateUrl: './interrumpcion-eventos.component.html',
+  styleUrl: './interrumpcion-eventos.component.css'
+})
+export class InterrumpcionEventosComponent {
+@Input() tiempo!: String;
+@Input() atendido_Por!: String;
+
+alerta =signal('alerta');
+borrado= false
+copiado= false
+deleteData(){
+  this.alerta.set('');
+  this.borrado= true;
+  setTimeout(() => this.borrado = false, 2000);
+}
+
+
+    alertaChangeHandler (event: Event) {
+      const input = event.target as HTMLInputElement;
+      const newValue = input.value.trim();
+      this.alerta.set(newValue);
+}
+
+copyContent(element: HTMLElement) {
+  const text = element.innerText;
+  navigator.clipboard.writeText(text).then(() => {
+    this.copiado = true;
+    setTimeout(() => this.copiado = false, 2000);
+  });
+}
+
+}
--- src/app/pages/window/window.component.ts
+++ src/app/pages/window/window.component.ts
@@ -4,18 +4,20 @@ import { CommonModule } from '@angular/common';
 import {AcpsComponent} from '../acps/acps.component'
 import { AbcComponent } from "../abc/abc.component";
 import { BruteforceComponent } from "../bruteforce/bruteforce.component";
+import { InterrumpcionEventosComponent } from "../interrumpcion-eventos/interrumpcion-eventos.component";
+import { TableSpaceComponent } from "../table-space/table-space.component";
 
 
 @Component({
   selector: 'app-window',
-  imports: [CommonModule, AcpsComponent, VpnsComponent, AbcComponent, BruteforceComponent],
+  imports: [CommonModule, AcpsComponent, VpnsComponent, AbcComponent, BruteforceComponent, InterrumpcionEventosComponent, TableSpaceComponent],
   templateUrl: './window.component.html',
   styleUrl: './window.component.css'
 })
 export class WindowComponent {
 
   alerta= signal<string>('Inicio');
-  alertas: string[] = ['Inicio','VPN', 'ABC', 'ACPS','Brute Force (.50/.100)'];
+  alertas: string[] = ['Inicio','VPN', 'ABC', 'ACPS','Brute Force (.50/.100)', 'Interrupción de Eventos', 'Table Space'];
 
 
   tiempo = signal<string>('Buenos días');
