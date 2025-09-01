ea59205cd5888875443bfc8754a5ae54b2087e71
dc10r <cg.david.0406@gmail.com>
2025-08-31 20:13:06 -0600

    'Version1.08.31'

--- src/app/pages/interrumpcion-eventos/interrumpcion-eventos.component.ts
+++ src/app/pages/interrumpcion-eventos/interrumpcion-eventos.component.ts
@@ -43,11 +43,57 @@ deleteData(){
 
 
 copyContent(element: HTMLElement) {
-  const text = element.innerText;
-  navigator.clipboard.writeText(text).then(() => {
-    this.copiado = true;
-    setTimeout(() => this.copiado = false, 2000);
-  });
+  const text =
+  `
+<style>
+table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    border: 2px solid rgb(0, 0, 0);
+    text-align: center;
+    height: 100px;
 }
+th{
+    background-color: rgb(0, 168, 224);
+    color: white;
+    border: 2px solid rgb(0, 0, 0);
+
+}
+td {
+    border: 2px solid rgb(0, 0, 0);
+}
+p{
+font-family: Arial, Helvetica, sans-serif;
+}
+.mailtable {
+    height: 350px;
+}
+
+.datos{
+    padding: 3px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+.datosTable{
+    padding: 3px;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+}
+  </style>
+    ${element.outerHTML}
+  `;
+const blob = new Blob([text], { type: 'text/html' });
+  const data = [new ClipboardItem({ 'text/html': blob })];
+
+  navigator.clipboard.write(data)
+    .then(() => console.log('Contenido copiado con éxito'))
+    .catch(err => console.error('Error al copiar:', err));
+    this.copiado= true;
+    setTimeout(() => this.copiado = false, 2000);
+}
+
+
 
 }
